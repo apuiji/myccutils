16b62a687911d11c3f8d5b3af3daca03d765a39e
apuiji <2330683684@qq.com>
2023-12-26 08:04:37 +0000

    a

---
+++ xyz.hh
@@ -0,0 +1,73 @@
+#pragma once
+
+#include<concepts>
+
+namespace zlt {
+  template<class ...T>
+  struct Dynamicastable {
+    template<class U>
+    requires (std::is_pointer_v<U>)
+    bool operator ()(const U u) noexcept {
+      return u && (dynamic_cast<const T*>(u) || ...);
+    }
+    template<class U>
+    requires (!std::is_pointer_v<U>)
+    bool operator ()(const U &u) noexcept {
+      return operator ()(&u);
+    }
+  };
+
+  template<class T, class ...U>
+  static constexpr bool isAnyOf = (std::is_same_v<T, U> || ...);
+
+  template<class T, class ...U>
+  concept AnyOf = isAnyOf<T, U...>;
+
+  template<std::invocable T>
+  static inline auto makeGuard(T &&t) noexcept {
+    struct Guard {
+      T t;
+      Guard(T &&t) noexcept: t(std::move(t)) {}
+      ~Guard() {
+        t();
+      }
+    };
+  }
+
+  /// overloaded function resolve
+  template<class R, class ...Args>
+  static inline constexpr auto ofr(R (*f)(Args...)) noexcept {
+    return f;
+  }
+
+  template<class T>
+  static inline T remove(T &&t) noexcept {
+    return std::move(t);
+  }
+
+  template<class T>
+  requires (!std::is_reference_v<T>)
+  static inline constexpr auto rtol(T t) noexcept {
+    struct U {
+      T t;
+      U(T t) noexcept: t(t) {}
+      operator T &() && noexcept {
+        return t;
+      }
+    };
+    return U(t);
+  }
+
+  template<class T>
+  requires (std::is_rvalue_reference_v<T>)
+  static inline auto rtol(T &&t) noexcept {
+    struct U {
+      T t;
+      U(T &&t) noexcept: t(std::move(t)) {}
+      operator T &() && noexcept {
+        return t;
+      }
+    };
+    return U(std::move(t));
+  }
+}
