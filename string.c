dbe9debe4cb010a218a22e05fcd98a1a1305980e
apuiji <2330683684@qq.com>
2024-06-06 16:53:50 +0800

    a

--- string.c
+++ string.c
@@ -2,7 +2,7 @@
 #include<math.h>
 #include"zlt/string.h"
 
-int zltIsBasedDigitChar(int c, size_t base) {
+int zltIsDigitChar(int c, size_t base) {
   if (base >= 2 && base < 10) {
     return c >= '0' && c < '0' + base ? c - '0' : -1;
   }
@@ -11,14 +11,14 @@ int zltIsBasedDigitChar(int c, size_t base) {
       return c - '0';
     }
     if (c >= 'A' && c < 'A' + base - 10) {
-      return c - 'A';
+      return c - 'A' + 10;
     }
     if (c >= 'a' && c < 'a' + base - 10) {
-      return c - 'a';
+      return c - 'a' + 10;
     }
     return -1;
   }
-  return zltIsDigitChar(c);
+  return -1;
 }
 
 zltString zltStrTrimStart(zltString str) {
@@ -44,29 +44,29 @@ zltString zltStrToLong(long *dest, zltString src, size_t base) {
     return src;
   }
   if (*src.data == '+') {
-    if (!(src.size > 1 && zltIsBasedDigitChar(src.data[1], base))) {
+    if (!(src.size > 1 && zltIsDigitChar(src.data[1], base))) {
       return src;
     }
-    return zltStrToUnsignedLong((unsigned long *) dest, zltStrForward(src, 1), base);
+    return zltStrToULong((unsigned long *) dest, zltStrForward(src, 1), base);
   }
   if (*src.data == '-') {
-    if (!(src.size > 1 && zltIsBasedDigitChar(src.data[1], base))) {
+    if (!(src.size > 1 && zltIsDigitChar(src.data[1], base))) {
       return src;
     }
-    zltString s = zltStrToUnsignedLong((unsigned long *) dest, zltStrForward(src, 1), base);
+    zltString s = zltStrToULong((unsigned long *) dest, zltStrForward(src, 1), base);
     *dest = -*dest;
     return s;
   }
-  return zltStrToUnsignedLong((unsigned long *) dest, src, base);
+  return zltStrToULong((unsigned long *) dest, src, base);
 }
 
-zltString zltStrToUnsignedLong(unsigned long *dest, zltString src, size_t base) {
+zltString zltStrToULong(unsigned long *dest, zltString src, size_t base) {
   for (; src.size; src = zltStrForward(src, 1)) {
-    int i = zltIsBasedDigitChar(*src.data, base);
+    int i = zltIsDigitChar(*src.data, base);
     if (i < 0) {
       break;
     }
-    *dest = (*dest * base) | i;
+    *dest = (*dest * base) + i;
   }
   return src;
 }
@@ -79,24 +79,24 @@ zltString zltStrToDouble(double *dest, zltString src) {
     if (!(src.size > 1 && isdigit(src.data[1]))) {
       return src;
     }
-    return zltStrToUnsignedDouble(dest, zltStrForward(src, 1));
+    return zltStrToUDouble(dest, zltStrForward(src, 1));
   }
   if (*src.data == '-') {
     if (!(src.size > 1 && isdigit(src.data[1]))) {
       return src;
     }
-    zltString s = zltStrToUnsignedDouble(dest, zltStrForward(src, 1));
+    zltString s = zltStrToUDouble(dest, zltStrForward(src, 1));
     *dest = -*dest;
     return s;
   }
-  return zltStrToUnsignedDouble(dest, src);
+  return zltStrToUDouble(dest, src);
 }
 
 static zltString strtoud1(double *dest, zltString src);
 static zltString strtoud2(double *dest, zltString src);
 static zltString strtoud3(double *dest, zltString src);
 
-zltString zltStrToUnsignedDouble(double *dest, zltString src) {
+zltString zltStrToUDouble(double *dest, zltString src) {
   zltString s = strtoud1(dest, src);
   return s.data ? strtoud3(dest, s) : src;
 }
@@ -107,7 +107,7 @@ zltString strtoud1(double *dest, zltString src) {
   }
   if (isdigit(*src.data)) {
     unsigned long l = 0;
-    zltString s = zltStrToUnsignedLong(&l, src, 10);
+    zltString s = zltStrToULong(&l, src, 10);
     *dest += l;
     return s.size && *s.data == '.' ? strtoud2(dest, zltStrForward(s, 1)) : s;
   }
@@ -119,7 +119,7 @@ zltString strtoud1(double *dest, zltString src) {
 
 zltString strtoud2(double *dest, zltString src) {
   unsigned long l = 0;
-  zltString s = zltStrToUnsignedLong(&l, src, 10);
+  zltString s = zltStrToULong(&l, src, 10);
   *dest += l / pow(10, src.size - s.size);
   return s;
 }
