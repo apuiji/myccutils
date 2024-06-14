d35537c21cc69e30c84d08495e0de8a0a884a628
apuiji <2330683684@qq.com>
2024-06-14 10:30:56 +0800

    a

--- Makefile
+++ Makefile
@@ -1,1 +1,1 @@
-OBJS = bitree.o io.o link.o rbtree.o sizetree.o stack.o string.o strtree.o xyz.o
+OBJS = bitree.o link.o rbtree.o sizetree.o stack.o string.o strtree.o xyz.o
--- io.c
+++
@@ -1,9 +0,0 @@
-#include"zlt/io.h"
-
-void zltFputs(FILE *dest, zltString s) {
-  const char *it = s.data;
-  size_t left = s.size;
-  for (; left; ++it, --left) {
-    fputc(*it, dest);
-  }
-}
