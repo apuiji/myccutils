966a595cc20c787a92377b69dd16489a7a991551
apuiji <2330683684@qq.com>
2024-06-27 17:14:28 +0800

    a

---
+++ strbuild.c
@@ -0,0 +1,12 @@
+#include"zlt/strbuild.h"
+
+bool zltStrBuildWrite(zltStack *dest, zltStack *buf, const void *data, size_t size) {
+  if (!size) {
+    return true;
+  }
+  if (size <= buf->left) {
+    zltStackPush(buf, data, size);
+  }
+}
+
+bool zltStrBuildFlush(zltStack *dest, zltStack *buf);
